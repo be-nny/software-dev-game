b6915a197f996c20d59f4f1cae05beb395f6cdd3
benabbott <ben.abbott1717@gmail.com>
2023-11-02 14:49:50 +0000

    setting up player pointers

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     private static ArrayList<Player> players = new ArrayList<>();
-    private static ArrayList<Deck> decks = new ArrayList<>();
+    private static volatile ArrayList<Deck> decks = new ArrayList<>();
     private static Pack pack = new Pack();
 
     public static void main(String[] args) {
@@ -14,12 +14,25 @@ public class Main {
     }
 
     private static void setupGame(int numPlayers){
+        // setting up player and deck lists
         for (int i=0; i < numPlayers; i++){
             String name = "Player " + i;
             players.add(new Player(name));
             decks.add(new Deck());
         }
 
-        //
+        // assigning discard and draw decks to players
+        for (int i=0; i<decks.size(); i ++){;
+            int discard;
+            if (i+1 < decks.size()){
+                discard = i+1;
+            } else {
+                discard = 0;
+            }
+
+            // assigning the pointers
+            players.get(i).setDrawDeckPointer(i);
+            players.get(i).setDiscardDeckPointer(discard);
+        }
     }
 }
