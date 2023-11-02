6ae7f9b7f2999a1b74e0adc0396a07681408ffc7
benabbott <ben.abbott1717@gmail.com>
2023-11-02 14:36:34 +0000

    setting up player pointers

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,18 @@
 public class Player {
-    public Player(){
 
+    private String name;
+    private int drawDeckPointer;
+    private int discardDeckPointer;
+
+    public Player(String name){
+        this.name = name;
+    }
+
+    public void setDrawDeckPointer(int pointer){
+        this.drawDeckPointer = pointer;
+    }
+
+    public void setDiscardDeckPointer(int pointer){
+        this.discardDeckPointer = pointer;
     }
 }
