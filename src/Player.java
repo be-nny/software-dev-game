d213387a18f216502d95e7d6a9b88c0285ae88bc
benabbott <ben.abbott1717@gmail.com>
2023-11-03 15:12:07 +0000

    documentation strings

--- src/Player.java
+++ src/Player.java
@@ -7,17 +7,33 @@ public class Player {
     private int discardDeckPointer;
     private ArrayList<Card> hand = new ArrayList<>();
 
-    public Player(String name, Pack pack){
+    /**
+     * @param name name of the player
+     * */
+    public Player(String name){
         this.name = name;
     }
+
+    /**
+     * Adds a card to the players hand
+     * @param card the card to be added to the players hand
+     * */
     public void addCard(Card card){
         this.hand.add(card);
     }
 
+    /**
+     * Sets an integer value pointer which corresponds to the deck the player draws to
+     * @param pointer integer pointer to deck
+     * */
     public void setDrawDeckPointer(int pointer){
         this.drawDeckPointer = pointer;
     }
 
+    /**
+     * Sets an integer value pointer which corresponds to the deck the player discards to
+     * @param pointer integer point to deck
+     * */
     public void setDiscardDeckPointer(int pointer){
         this.discardDeckPointer = pointer;
     }
