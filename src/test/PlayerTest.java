bc4f8a30c597a9b03ebc1686a4c60bb483db96f5
Ellis <ellismackness04@gmail.com>
2023-11-23 12:22:01 +0000

    Player tests (private methods)

--- src/test/PlayerTest.java
+++ src/test/PlayerTest.java
@@ -1,5 +1,4 @@
 package test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,6 +11,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 public class PlayerTest {
     private Deck deck;
@@ -76,6 +76,51 @@ public class PlayerTest {
         } catch (IllegalAccessException | InvocationTargetException e) {
             throw new RuntimeException(e);
         }
+
+    }
+    @Test
+    public void isWinTest() throws NoSuchFieldException {
+        String methodName = "isWin";
+
+        Field handField = Player.class.getDeclaredField("hand");
+        handField.setAccessible(true);
+
+        player = new Player("Player1", 1);
+        for (int i = 1; i<5; i++){
+            Card card = mock(Card.class);
+            when(card.getFaceValue()).thenReturn(1);
+            player.addCard(card);
+        }
+
+        assertDoesNotThrow(() -> {
+            Method playerIsWinMethod = Player.class.getDeclaredMethod(methodName);
+            playerIsWinMethod.setAccessible(true);
+
+            playerIsWinMethod.invoke(player);
+            boolean result = (boolean) playerIsWinMethod.invoke(player);
+            assertTrue(result);
+        });
+    }
+
+    @Test
+    public void notifyWinListenerTest() throws NoSuchFieldException, IllegalAccessException{
+        String methodName = "notifyWinListener";
+
+        Field listenersField = Player.class.getDeclaredField("listeners");
+        listenersField.setAccessible(true);
+
+        ArrayList<WinListener> listeners = new ArrayList<>();
+        WinListener mockListener1 = mock(WinListener.class);
+        listeners.add(mockListener1);
+        listenersField.set(player, listeners);
+
+        assertDoesNotThrow(() -> {
+            Method playerNotifyTest = Player.class.getDeclaredMethod(methodName);
+            playerNotifyTest.setAccessible(true);
+
+            playerNotifyTest.invoke(player);
+            verify(mockListener1).notifyPlayerWon(player);
+        });
 
     }
 }
